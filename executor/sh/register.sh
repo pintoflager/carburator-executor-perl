0cd4347708d706727ddf36159d8b0202f9113de2
pintoflager <jaakko0606@gmail.com>
2023-06-13 10:28:11 -0500

    try app register

--- executor/sh/register.sh
+++ executor/sh/register.sh
@@ -2,44 +2,80 @@
 
 role="$1"
 
-# App installation tasks on a client node. Runs first
-if [ "$role" = 'client' ]; then
-    carburator print terminal info "Executing install script on a client"
-fi
-
-# App installation tasks on remote server node.
-if [ "$role" = 'server' ]; then
-    carburator print terminal info "Executing install script on a server"
-fi
-
-# Test if we already have perl
+# If perl is present don't bother with the install
 if ! carburator has program perl; then
-    carburator print terminal error \
-        "Missing required program perl. Trying to install..."
+    carburator print terminal warn \
+        "Missing required program perl. Trying to install with package manager..."
 else
-    carburator print terminal success "Perl found from the $role"
+    carburator print terminal success "perl found from the $role node."
     exit 0
 fi
 
-# TODO: Untested below
-if carburator has program apt; then
-    apt-get -y update
-    apt-get -y install perl
+# App installation tasks on a client node. Runs first, runs as normal user.
+if [ "$role" = 'client' ]; then
+    carburator print terminal info \
+        "Executing register script on a client node"
 
-elif carburator has program pacman; then
-    pacman update
-    pacman -Sy perl
+    # TODO: Untested below
+    if carburator has program apt; then
+        sudo apt-get -y update
+        sudo apt-get -y install perl
 
-elif carburator has program yum; then
-    yum makecache --refresh
-    yum install perl
+    elif carburator has program pacman; then
+        sudo pacman update
+        sudo pacman -Sy perl
 
-elif carburator has program dnf; then
-    dnf makecache --refresh
-    dnf -y install perl-core
+    elif carburator has program yum; then
+        sudo yum makecache --refresh
+        sudo yum install epel-release
+        sudo yum install perl
 
-else
-    carburator print terminal error \
-        "Unable to detect package manager from client node linux"
-    exit 120
+    elif carburator has program dnf; then
+        sudo dnf makecache --refresh
+        sudo dnf -y install perl-core
+
+    else
+        carburator print terminal error \
+            "Unable to detect package manager from localhost OS"
+        exit 120
+    fi    
+fi
+
+# App installation tasks on remote server node. Runs as root.
+if [ "$role" = 'server' ]; then
+    carburator print terminal info \
+        "Executing register script on a server node"
+
+    carburator prompt yes-no \
+        "Should we try to install perl on your computer?" \
+            --yes-val "Yes try to install with a script" \
+            --no-val "No, I'll install everything myself"; exitcode=$?
+
+    if [ $exitcode -ne 0 ]; then
+        exit 120
+    fi
+
+    # TODO: Untested below
+    if carburator has program apt; then
+        apt-get -y update
+        apt-get -y install perl
+
+    elif carburator has program pacman; then
+        pacman update
+        pacman -Sy perl
+
+    elif carburator has program yum; then
+        yum makecache --refresh
+        yum install epel-release
+        yum install perl
+
+    elif carburator has program dnf; then
+        dnf makecache --refresh
+        dnf -y install perl-core
+
+    else
+        carburator print terminal error \
+            "Unable to detect package manager from server node linux"
+        exit 120
+    fi
 fi
